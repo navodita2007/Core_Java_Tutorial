4f664f7a552f34d25b78297ac9d42fa348380b46
Navodita <navoditaranchi@gmail.com>
2025-02-24 16:34:21 +0200

    Scanner class program

---
+++ src/ReadFromKeyboard.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+// This code will read an integer and then display an integer
+public class ReadFromKeyboard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
+
+    }
+
+}
