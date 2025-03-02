eeee325fdff7802cb29222cc5cee65f35d70ebf4
Navodita <navoditaranchi@gmail.com>
2025-03-02 17:11:50 +0200

    program about arithmetic operation

---
+++ src/Arithmetic.java
@@ -0,0 +1,12 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 14, b = 5;
+
+        // int c = a / b;   //2
+        float c = (float) a / b;
+        int r = a % b; //4
+
+        System.out.println(c);
+        System.out.println(r);
+    }
+}
