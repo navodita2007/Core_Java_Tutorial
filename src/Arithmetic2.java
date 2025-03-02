c7acae3565833ee1b87e0f49b729619872621191
Navodita <navoditaranchi@gmail.com>
2025-03-02 17:41:35 +0200

    Arithmetic program

---
+++ src/Arithmetic2.java
@@ -0,0 +1,10 @@
+public class Arithmetic2 {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 15;
+
+        // byte c= a+b; this will give error
+        int c = a + b;
+        System.out.println(c);
+    }
+}
