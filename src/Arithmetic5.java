37f7bbf597f7c0bda647e87de97545bf3ff78a27
Navodita <navoditaranchi@gmail.com>
2025-03-02 18:29:09 +0200

    Arithmetic program

---
+++ src/Arithmetic5.java
@@ -0,0 +1,11 @@
+public class Arithmetic5 {
+    public static void main(String[] args) {
+        char a = 40;
+        int b = 30;
+
+        int c = a - b;
+        // char c = a - b;  this will give error because it is not allowed
+
+        System.out.println(c);
+    }
+}
