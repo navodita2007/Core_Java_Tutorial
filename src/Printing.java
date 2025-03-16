c5a7c85395161e003019a438fdeb3e35f7bae649
Navodita <navoditaranchi@gmail.com>
2025-03-16 17:08:59 +0200

    Code about Format Specifier

---
+++ src/Printing.java
@@ -0,0 +1,9 @@
+public class Printing {
+    public static void main(String[] args) {
+        int x = 10;
+        float y = 12.56f;
+        char z = 'A';
+        System.out.printf("Hello %d %f %c World\n", x, y, z);
+    }
+}
+// Format specifiers :-    %d -> for integer , %f -> fpr float , %c -> for character
