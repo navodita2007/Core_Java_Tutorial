8bc614de98de564052217fd5f96f4b7052001581
Navodita <navoditaranchi@gmail.com>
2025-03-17 12:18:59 +0200

    code using flags, width

---
+++ src/Formatting.java
@@ -0,0 +1,7 @@
+public class Formatting {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.printf("%05d", a);
+        // 5 -> width i.e., spaces  ;  0->flag i.e., fill empty spaces with zeros
+    }
+}
