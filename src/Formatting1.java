4d7ccd332fa13c93187b98ad2b05b492aff319d7
Navodita <navoditaranchi@gmail.com>
2025-03-17 14:39:17 +0200

    code using flag (

---
+++ src/Formatting1.java
@@ -0,0 +1,7 @@
+public class Formatting1 {
+    public static void main(String[] args) {
+        int a = -10;
+        System.out.printf("%(5d", a);
+        // if number is negative i.e., here -10, then ( is used in format specifier
+    }
+}
